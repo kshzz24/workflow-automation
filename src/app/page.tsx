0ae5fa8a7b131fe654aac172e075d6f9cfd57399
Kshitiz Bartaria <kshitizb02@gmail.com>
2025-11-21 21:01:40 +0530

    added trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-const Page = () => {
-  return <div className="text-red-500">Hello</div>;
+import { caller } from "@/trpc/server";
+
+const Page = async () => {
+  const users = await caller.getUsers();
+  return <div className="text-red-500">{JSON.stringify(users)}</div>;
 };
 
 export default Page;
