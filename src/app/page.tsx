303baf7dcddd6270ebf1a16843ce55a50860f776
Kshitiz Bartaria <kshitizb02@gmail.com>
2025-11-22 19:16:16 +0530

    add inngest cron job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
+"use client";
+import LogoutButton from "./logout";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
-import { requireAuth } from "@/lib/auth-utils";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const queryClient = useQueryClient();
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      protected server component
-      <Button>Logout</Button>
+      {JSON.stringify(data, null, 2)}
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
+      <LogoutButton />
     </div>
   );
 };
