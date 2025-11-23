6e843b7ba676619ba8c6706569b24411fbd4f99b
Kshitiz Bartaria <kshitizb02@gmail.com>
2025-11-23 13:56:04 +0530

    add worfklows crud

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,9 +1,27 @@
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
-import React from "react";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import React, { Suspense } from "react";
+import {
+  WorkflowsContainers,
+  WorkflowsList,
+} from "@/features/workflows/components/workflows";
 
 const Page = async () => {
   await requireAuth();
-  return <p>Workflows</p>;
+  prefetchWorkflows();
+  return (
+    <WorkflowsContainers>
+      <HydrateClient>
+        <ErrorBoundary fallback={<p>Error</p>}>
+          <Suspense fallback={<p>Loading</p>}>
+            <WorkflowsList />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </WorkflowsContainers>
+  );
 };
 
 export default Page;
