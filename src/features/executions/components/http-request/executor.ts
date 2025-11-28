d7fc7089e322f2cbf1ec781cab7a894f14698821
kshitizb <kshitiz@seclogic.io>
2025-11-28 05:32:03 +0530

    templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,11 +1,18 @@
 import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
+import Handlebars from "handlebars";
+
+Handlebars.registerHelper("json", function (context) {
+  const stringified = JSON.stringify(context, null, 2);
+  const safeString = new Handlebars.SafeString(stringified);
+  return safeString;
+});
 
 type HttpRequestData = {
-  variableName?: string;
-  endpoint?: string;
-  method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  variableName: string;
+  endpoint: string;
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
   body?: string;
 };
 
@@ -23,18 +30,24 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     throw new NonRetriableError("Variable name is required");
   }
 
+  if (!data.method) {
+    throw new NonRetriableError("Method is required");
+  }
+
   const result = await step.run("http-request", async () => {
-    const method = data.method || "GET";
-    const endpoint = data.endpoint;
+    const method = data.method;
+    const endpoint = Handlebars.compile(data.endpoint)(context);
     const options: KyOptions = { method };
 
     if (["POST", "PUT", "PATCH"].includes(method)) {
-      options.body = data.body;
+      const resolved = Handlebars.compile(data.body || "{}")(context);
+      JSON.parse(resolved);
+      options.body = resolved;
       options.headers = {
         "Content-Type": "application/json",
       };
     }
-    const response = await ky(endpoint!, options);
+    const response = await ky(endpoint, options);
     const contentType = response.headers.get("content-type");
     let responseData;
     if (contentType?.includes("application/json")) {
@@ -51,16 +64,9 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       },
     };
 
-    if (data.variableName) {
-      return {
-        ...context,
-        [data.variableName]: responsePayload,
-      };
-    }
-
     return {
       ...context,
-      ...responsePayload,
+      [data.variableName]: responsePayload,
     };
   });
 
